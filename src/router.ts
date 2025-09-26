e29e5b91c83c18809c962dd6782ac03bba6e22d7
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-09-26 09:43:54 -0700

    Add update point routes to router
    Introduced CRUD routes for the /updatepoint endpoint, including GET, PUT, POST, and DELETE handlers. This expands the API to support operations on update points.

--- src/router.ts
+++ src/router.ts
@@ -16,3 +16,11 @@ router.get("/update/:id", () => {});
 router.put("/update/:id", () => {});
 router.post("/update", () => {});
 router.delete("/update/:id", () => {});
+
+
+// update point routes
+router.get("/updatepoint", () => {});
+router.get("/updatepoint/:id", () => { });
+router.put("/updatepoint/:id", () => { });
+router.post("/updatepoint", () => { });
+router.delete("/updatepoint/:id", () => { });
