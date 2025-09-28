4a10ff25382128249f07b3ec7b0a64ea43126b13
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-09-28 11:35:04 -0700

    Add CORS middleware to Express server
    Imported and applied the 'cors' middleware to enable Cross-Origin Resource Sharing for the API.

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import morgan from "morgan";
 import router from "./router";
+import cors from "cors";
 
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 
  
