bbd5b7ec88998aa00841b01038b9582b166b6f5f
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-10-03 10:37:15 -0700

    Implement create and update handlers for updates
    Added logic to create and update 'update' records in src/handlers/update.ts, including user ownership checks. Also made the 'status' field optional in the update route validation in src/router.ts.

--- src/handlers/update.ts
+++ src/handlers/update.ts
@@ -1,29 +1,67 @@
-import prisma from '../db'
+import prisma from "../db";
 
 export const getOneUpdate = async (req, res) => {
-    const update = await prisma.update.findUnique({
-        where: {
-            id: req.params.id
-        }
-    })
+  const update = await prisma.update.findUnique({
+    where: {
+      id: req.params.id,
+    },
+  });
 
-    res.json({data: update})
- };
-export const getUpdates = async (req, res) => {
-    const products = await prisma.product.findMany({
-        where: {
-          belongsToId: req.user.id  
-        },
-        include: {
-            updates: true
-        }
-    })
-    const updates = products.reduce((allUpdates, product) => {
-        return  [...allUpdates, ...product.updates]
-    }, [])
-
-    res.json({data: updates})
+  res.json({ data: update });
 };
-export const createUpdate = async (req, res) => {};
-export const updateUpdate = async (req, res) => {};
+export const getUpdates = async (req, res) => {
+  const products = await prisma.product.findMany({
+    where: {
+      belongsToId: req.user.id,
+    },
+    include: {
+      updates: true,
+    },
+  });
+  const updates = products.reduce((allUpdates, product) => {
+    return [...allUpdates, ...product.updates];
+  }, []);
+
+  res.json({ data: updates });
+};
+export const createUpdate = async (req, res) => {
+  const product = await prisma.product.findUnique({
+    where: {
+      id: req.body.id,
+    },
+  });
+  if (!product) {
+    // does not belong to user
+    return res.json({ message: "nope" });
+  }
+
+  const update = await prisma.update.create({
+    data: req.body,
+  });
+
+  res.json({ data: update });
+};
+export const updateUpdate = async (req, res) => {
+  const products = await prisma.product.findMany({
+    where: {
+      belongsToId: req.user.id,
+    },
+    include: {
+      updates: true,
+    },
+  });
+
+  const updates = products.reduce((allUpdates, product) => {
+    return [...allUpdates, ...product.updates];
+  }, []);
+
+  const match = updates.find((update) => update.id === req.params.id);
+
+  if (!match) {
+    // handle this
+    return res.json({ message: "nope" });
+  }
+
+  const updateUpdate = await prisma.update.update({});
+};
 export const deleteUpdate = async (req, res) => {};
